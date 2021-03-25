b725fd9ac800ccb31cad520627ff4ed5e8db4998
SulemanBajwa <58045793+suleman81@users.noreply.github.com>
2021-03-25 21:15:19 +0500

    First commit

---
+++ src/Nav.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Nav.css";
+
+const Nav = () => {
+  return (
+    <div className="nav">
+      <img src="./logo.png" className="nav_logo" alt="logo" />
+    </div>
+  );
+};
+
+export default Nav;
